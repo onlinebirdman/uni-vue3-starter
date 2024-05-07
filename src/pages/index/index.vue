9cdfa1c5136cc07795f14fc7647f37ff07b30986
jp <137669276@qq.com>
2024-05-07 16:30:07 +0800

    feat add unocss support

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,45 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text color="red" class="color-green">{{ title }}</text>
+    <view class="grid grid-cols-3 justify-items-center">
+      <view
+        v-for="(item, index) in list" :key="index" class="w-200 h-200 shadow-md center rounded-md"
+      >
+        {{ item.name }}
+      </view>
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
+const list = [
+  { name: 'font', path: '/pages/font/index' },
+  { name: 'size', path: '/pages/size/index' },
+  { name: 'shadow', path: '/pages/shadow/index' },
+  { name: 'border', path: '/pages/border/index' },
+  { name: 'spacing', path: '/pages/spacing/index' },
+  { name: 'shortcuts', path: '/pages/shortcuts/index' },
+  { name: ':class', path: '/pages/class/index' },
+  { name: 'color', path: '/pages/color/index' },
+  { name: 'attributify', path: '/pages/attributify-mode/index' },
+  { name: 'animation', path: '/pages/animation/index' },
+  { name: 'bg', path: '/pages/bg/index' },
+  { name: 'first-last', path: '/pages/first-last/index' },
+  { name: 'odd-even', path: '/pages/odd-even/index' },
+  { name: 'space-x', path: '/pages/space/index' },
+  { name: 'divide-x', path: '/pages/divide/index' },
+  { name: 'apply', path: '/pages/apply/index' },
+  { name: 'icon', path: '/pages/icon/index' },
+]
+
+function to(to: string) {
+  uni.navigateTo({
+    url: to,
+  })
+}
+
+const loading = ref(false)
 const title = ref('Hello')
 </script>
 
